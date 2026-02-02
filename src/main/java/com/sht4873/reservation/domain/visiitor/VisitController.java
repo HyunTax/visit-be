5eb3106357aea5d1d4d4d4f79340386a1a64cb98
HyunTax <sht4873@nate.com>
2026-02-02 21:10:01 +0900

    커스텀 에러 처리

--- src/main/java/com/sht4873/reservation/domain/visiitor/VisitController.java
+++ src/main/java/com/sht4873/reservation/domain/visiitor/VisitController.java
@@ -21,13 +21,13 @@ public class VisitController {
     }
 
     @PostMapping
-    public ResponseEntity<ReservationResponse> reservation(@RequestBody ReservationRequest request) throws Exception {
+    public ResponseEntity<ReservationResponse> reservation(@RequestBody ReservationRequest request) {
         Visit reservation = service.reservation(Visit.convertEntity(request));
         return ResponseEntity.ok(ReservationResponse.convert(reservation));
     }
 
     @GetMapping("/find")
-    public ResponseEntity<ReservationResponse> findReservation(ReservationSearchRequest request) throws Exception {
+    public ResponseEntity<ReservationResponse> findReservation(ReservationSearchRequest request) {
         Visit reservation = service.findReservation(request);
         return ResponseEntity.ok(ReservationResponse.convert(reservation));
     }
@@ -39,13 +39,13 @@ public class VisitController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ReservationResponse> updateReservation(@PathVariable Long id, @RequestBody ReservationRequest request) throws Exception {
+    public ResponseEntity<ReservationResponse> updateReservation(@PathVariable Long id, @RequestBody ReservationRequest request) {
         Visit reservation = service.updateReservation(id, Visit.convertEntity(request, "name", "phoneNum", "password"));
         return ResponseEntity.ok(ReservationResponse.convert(reservation));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> cancelReservation(@PathVariable Long id) throws Exception {
+    public ResponseEntity<Void> cancelReservation(@PathVariable Long id) {
         service.cancelReservation(id);
         return ResponseEntity.ok().build();
     }
