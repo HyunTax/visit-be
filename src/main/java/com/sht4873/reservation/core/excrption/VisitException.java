905bfe6bb34eb47943091df5b8980984ac74c4e6
HyunTax <sht4873@nate.com>
2026-02-11 22:29:27 +0900

    Custom Exception 상태 코드

--- src/main/java/com/sht4873/reservation/core/excrption/VisitException.java
+++ src/main/java/com/sht4873/reservation/core/excrption/VisitException.java
@@ -1,7 +1,18 @@
 package com.sht4873.reservation.core.excrption;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class VisitException extends RuntimeException {
+    private final HttpStatus status;
+
     public VisitException(String message) {
+        this(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public VisitException(String message, HttpStatus status) {
         super(message);
+        this.status = status;
     }
 }
